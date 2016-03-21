e2040fe4e9bf302a3240e79431e00cc17f800066
BlackBull2015 <aroslaw007chudy@gmail.com>
2016-03-21 21:53:20 +0000

    Changed servo number and preparation for tests

--- left_Legs_with_atmega168_End_Code/left_Legs_with_atmega168_End_Code.ino
+++ left_Legs_with_atmega168_End_Code/left_Legs_with_atmega168_End_Code.ino
@@ -10,42 +10,32 @@
 #include <Servo.h>
 
 Servo Leg1Ser1; //servo for first leg servo no 1
-Servo Leg1Ser2; //servo for first leg servo no 2
-
 
 Servo Leg2Ser1; //servo for second leg servo no 1
-Servo Leg2Ser2; //servo for second leg servo no 2
-
 
 Servo Leg3Ser1; //servo for second leg servo no 1
-Servo Leg3Ser2; //servo for second leg servo no 2
 
 int Sensor1 = 1;
 int Sensor2 = 2;
 int Sensor3 = 3;
-int BIN1 = 5;
-int BIN2 = 6;
-int ACTION  = 4;
 
 int SensorDetect = 1;
 
 
 int leg1s1, leg2s1, leg3s1;
-int leg1s2, leg2s2, leg3s2;
 int minpos = 20;
 int maxpos = 180;
 int pos;
+int runFlag = 0;
+
+String message;
 
 void setup()
 {
-
+  Serial.begin(9600);
   pinMode(Sensor1, INPUT);
   pinMode(Sensor2, INPUT);
   pinMode(Sensor3, INPUT);
-
-  pinMode(BIN1, INPUT);
-  pinMode(BIN2, INPUT);
-  pinMode(ACTION, INPUT);
 
   initServos();
 }
@@ -53,29 +43,38 @@ void setup()
 void loop()
 {
 
-  while (ACTION != 0) {
-
-    //If accelerometer detects is the Left side up
-    if (digitalRead(BIN1) == 0 && digitalRead(BIN2) == 0 ) {
-      legUp1();
-      legUp2();
-    } else if (digitalRead(BIN1) == 1 && digitalRead(BIN2) == 0 ) {
-      legUp2();
-      legUp3();
-    } else if (digitalRead(BIN1) == 0 && digitalRead(BIN2) == 1 ) {
-      legUp1();
-      legUp2();
-      legUp3();
-    } else if (digitalRead(BIN1) == 1 && digitalRead(BIN2) == 1 ) {
-      legDown1();
-      legDown2();
-      legDown3();
-    } else {
-      sensorCheck();
-    }
-    delay(10);
+  if(Serial.available()){
+    message = Serial.readString();
   }
-  sensorCheck();
+
+if(message == "lock"){
+  runFlag = 0;
+}else if(message == "unlock"){
+  runFlag = 1;
+}
+
+
+  
+  if(runFlag == 1) {
+    
+      if(message == "forward"){
+  
+}else if(message == "reverse"){
+  
+}else if(message == "left"){
+  
+}else if(message == "right"){
+  
+}else if(message == "up"){
+  
+}else if(message == "down"){
+  
+}
+    
+      sensorCheck();
+      delay(10);
+  }
+
 }
 
 
@@ -94,63 +93,47 @@ void sensorCheck() {
 void legUp1()
 {
   Leg1Ser1.write(leg1s1++);
-  Leg1Ser2.write(leg1s2--);
 }
 
 void legDown1()
 {
   Leg1Ser1.write(leg1s1--);
-  Leg1Ser2.write(leg1s2++);
 }
 
 void legUp2()
 {
   Leg2Ser1.write(leg2s1++);
-  Leg2Ser2.write(leg2s2--);
-
 }
 
 void legDown2()
 {
   Leg2Ser1.write(leg2s1--);
-  Leg2Ser2.write(leg2s2++);
 }
 void legUp3()
 {
   Leg3Ser1.write(leg3s1++);
-  Leg3Ser2.write(leg3s2--);
 }
 
 void legDown3()
 {
   Leg3Ser1.write(leg3s1--);
-  Leg3Ser2.write(leg3s2++);
 }
 void initServos() {
   Leg1Ser1.attach(7);
-  Leg1Ser2.attach(8);
-  Leg2Ser1.attach(9);
-  Leg2Ser2.attach(10);
-  Leg3Ser1.attach(11);
-  Leg3Ser2.attach(12);
+  Leg2Ser1.attach(8);
+  Leg3Ser1.attach(9);
 
   leg1s1 = minpos;
-  leg1s2 = maxpos;
 
   leg2s1 = minpos;
-  leg2s2 = maxpos;
 
   leg3s1 = minpos;
-  leg3s2 = maxpos;
 
   Leg1Ser1.write(leg1s1);
-  Leg1Ser2.write(leg1s2);
   delay(25);
   Leg2Ser1.write(leg2s1);
-  Leg2Ser2.write(leg2s2);
   delay(25);
   Leg3Ser1.write(leg3s1);
-  Leg3Ser2.write(leg3s2);
   delay(25);
 }
 
